ef05b5a976f840e237c77a697f3b824febdf5fe5
Ryan DS <rdspring1@gmail.com>
2015-08-17 08:11:40 -0400

    Completed EKF TODO stubs - Not Working

--- ekf/python/ekf_framework.py
+++ ekf/python/ekf_framework.py
@@ -44,36 +44,77 @@ def read_sensor_data(filename):
     return data_dict
 
 def prediction_step(mu, sigma, odometry):
-    # TODO Read in the state from the mu vector
-    # TODO Read in the odometry i.e r1, t , r2
-    # TODO Compute the noise free motion.
-    # TODO Computing the Jacobian of G with respect to the state
+    # Read in the state from the mu vector
+    x = mu[0]
+    y = mu[1]
+    theta = mu[2]
 
-    # TODO Use the Motion Noise as given in the exercise
+    # Read in the odometry i.e r1, t , r2
+    r1 = odometry['r1']
+    t = odometry['t']
+    r2 = odometry['r2']
+
+    # Compute the noise free motion.
+    # calculate r1 + theta
+    motion_theta = theta + r1
+    # calculate x' and y'
+    x1 = x + cos(motion_theta) * t
+    y1 = y + sin(motion_theta) * t
+    # calculate theta' = r1 + r2 + theta
+    theta1 = motion_theta + r2
+
+    mu[0] = x1
+    mu[1] = y1
+    mu[2] = theta1
+
+    # Computing the Jacobian of G with respect to the state
+    G = [[0, 0, 0], [0, 0, 0], [-sin(motion_theta) * t, cos(motion_theta) * t, 0]]
+
+    # Use the Motion Noise as given in the exercise
     Q = [[0.2, 0,   0],
         [0,   0.2, 0],
         [0,   0,   0.02]]
 
-    # TODO Predict the covariance
+    # Predict the covariance
+    sigma = np.dot(np.dot(G, sigma), np.transpose(G)) + Q
     return mu, sigma
 
 
 def correction_step(mu, sigma, measurements, world_dict):
-    # TODO Get the states
-    # TODO Read in the ids and ranges  from measurements using dictionary indexing
+    # Get the states
+    x = mu[0]
+    y = mu[1]
+    theta = mu[2]
 
-    # Initialize the Jacobian of h
+    # Read in the ids and ranges from measurements using dictionary indexing
+    ids = measurements['id']
+    ranges = measurements['range']
+
+    # Initialize the Jacobian of H
     H = np.zeros((len(ids),3))
 
-    #Vectorize measurements
+    Zdelta = [0 for x in range(len(ids))]
+    # Vectorize measurements
     for i in range(len(ids)):
-          # TODO
-          # For each measurement , compute a row of H
-          #H[i,:]=
+        # For each measurement, compute a row of H -> H[i,:]=
+        delta = [(world_dict[int(ids[i])][0] - x), (world_dict[int(ids[i])][1] - y)]
+        z1 = pow(np.dot(delta, delta), 0.5)
+        H[i][0] = -delta[0]/z1
+        H[i][1] = -delta[1]/z1
+        Zdelta[i] = ranges[i] - z1
 
-     # TODO Noise covariance for the measurements
-     # TODO Kalman Gain
-     # TODO Kalman correction for mean and covariance
+    # Noise covariance for the measurements
+    R = np.diag([0.5 for x in range(len(ids))])
+
+    # Innovation Covariance
+    S = np.dot(np.dot(H, sigma), np.transpose(H)) + R
+
+    # Kalman Gain
+    K = np.dot(np.dot(sigma, np.transpose(H)), np.linalg.inv(S))
+
+    # Kalman correction for mean and covariance
+    mu = mu + np.dot(K, Zdelta)
+    sigma = np.dot((np.identity(len(mu)) - np.dot(K, H)), sigma)
     return mu,sigma
 
 ## Main loop Starts here
@@ -87,8 +128,8 @@ data_dict = read_sensor_data(args.sensor_data)
 world_data = read_world_data(args.world_data)
 
 #Initial Belief
-mu = np.array([0.0,0.0, 0.0]).T
-sigma =np.array([[1.0,0.0 , 0.0],[0.0, 1.0 , 0.0],[0.0, 0.0 , 1.0]])
+mu = np.array([0.0, 0.0, 0.0]).T
+sigma = np.array([[1.0, 0.0 , 0.0],[0.0, 1.0 , 0.0],[0.0, 0.0 , 1.0]])
 
 # Landmark Positions
 lx=[]
